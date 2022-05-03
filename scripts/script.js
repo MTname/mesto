55eb8c59e6ba01785c9aba9d4d64e201e86cf976
Mike <333.333.333@list.ru>
2022-05-03 18:51:50 +0500

    second iteration

--- scripts/script.js
+++ scripts/script.js
@@ -1,25 +1,31 @@
 const popup = document.querySelector('.popup');
 const popupForm = document.querySelector('.popup__box');
-const inputName = popupForm.querySelector('.popup__form-name');
-const inputAboutText = popupForm.querySelector('.popup__form-text');
+const inputName = popupForm.querySelector('.popup__form-input_name');
+const inputAboutText = popupForm.querySelector('.popup__form-input_text');
 const popupCloseBtn = document.querySelector('.popup__close-button');
-const popupOpenBtn = document.querySelector('.profile-info__edit-button');
-const profileName = document.querySelector('.profile-info__name');
-const profileAboutText = document.querySelector('.profile-info__text');
+const popupOpenBtn = document.querySelector('.profile__info_edit-button');
+const profileName = document.querySelector('.profile__info_name');
+const profileAboutText = document.querySelector('.profile__info_text');
 
-popupOpenBtn.addEventListener('click', function() {
+function popupOpen() {
     popup.classList.add('popup_active');
     inputName.value = profileName.textContent;
     inputAboutText.value = profileAboutText.textContent;
-});
+}
 
-popupCloseBtn.addEventListener('click', function() {
+popupOpenBtn.addEventListener('click', popupOpen);
+
+function popupClose() {
     popup.classList.remove('popup_active');
-});
+}
 
-popupForm.addEventListener('submit', function(evt) {
+popupCloseBtn.addEventListener('click', popupClose);
+
+function popupFormSubmit(evt) {
     evt.preventDefault();
     profileName.textContent = inputName.value;
     profileAboutText.textContent = inputAboutText.value;
-    popup.classList.remove('popup_active');
-});
+    popupClose();
+}
+
+popupForm.addEventListener('submit', popupFormSubmit);
